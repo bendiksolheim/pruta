2566c7fdf79987ce4478f4cbe1d804e92539283e
Bendik Solheim <hello@bendik.dev>
2022-10-29 21:57:03 +0200

    Add arrow-kt

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-cio-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
+    implementation("io.arrow-kt:arrow-core:1.1.2")
     implementation("com.github.docker-java:docker-java:3.2.13")
     testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
